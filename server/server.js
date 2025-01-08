1c3707732d37fa5628ecd4b01ca013aa3bb9d04a
malka-4325 <m0583257174@gmail.com>
2025-01-08 15:54:45 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require ("dotenv").config()
+const express=require("express")
+const cors=require("cors")
+const corsOptions=require("./config/corsOptions")
+const connectDB=require("./config/dbConn")
+const { default: mongoose } = require("mongoose")
+const PORT= process.env.PORT||1300
+const app=express()
+connectDB()
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.static("public"))
+
+// app.use("/api/posts",require("./routes/posts"))
+// app.use("/api/users",require("./routes/users"))
+// app.use("/api/todos",require("./routes/todos"))
+
+
+mongoose.connection.once('open',()=>{
+    console.log('connected to mongoDB')
+    app.listen(PORT,()=>{
+        console.log(`server running on port ${PORT}`)
+    })
+})
+mongoose.connection.on('error',err=>{
+    console.log(err);
+})
