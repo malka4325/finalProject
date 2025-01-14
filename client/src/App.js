b21cc85a99f22aa66d534d5052afc175bf3a3f5e
malka-4325 <m0583257174@gmail.com>
2025-01-14 14:34:06 +0200

    trying

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Trips from './Components/Trips/Trips';
 
 function App() {
   return (
     <div className="App">
- 
+ <Trips/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import './index.css';
+import './flags.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
